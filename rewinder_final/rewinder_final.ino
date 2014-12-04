5fe36959dd6bed91745a0f3dd3956db5713f90d8
Jeffrey C <jeff2435@gmail.com>
2014-12-04 05:09:20 -0800

    ALL the changes!

--- rewinder_final/rewinder_final.ino
+++ rewinder_final/rewinder_final.ino
@@ -1,18 +1,12 @@
-//#include <ATimerDefs.h>
-//#include <BTimerDefs.h>
-#include <PWM.h>
-
 // NewPing library is for the ultrasonic sensor
 #include <NewPing.h>
-// PWM library gives functions to enable the internal timers
-
 
 #define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
-int motor = 10; // Arduino pin tied to motor/MOS SIG pin on the ultrasonic sensor.
-int frequency = 1000; // A frequency for the PWM that works with the motor - needs tuning
+int motor = 0; // Arduino pin tied to motor/MOS SIG pin on the ultrasonic sensor.
+int feeder = 10;
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
@@ -23,17 +17,12 @@ NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and
 void setup() {
   Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
 
-  //initialize all timers except for 0, to save time keeping functions
-  InitTimersSafe(); 
-
-  //sets the frequency for the specified pin
-  bool success = SetPinFrequencySafe(motor, frequency);
-
   //if the pin frequency was set successfully, turn pin 10 on
-  if(success) {
     pinMode(motor, OUTPUT);
-    digitalWrite(motor, HIGH);    
-  }
+    analogWrite(motor, HIGH);    
+    
+    pinMode(feeder, OUTPUT);
+    digitalWrite(feeder, HIGH);
 }
 
 void loop() {
@@ -51,20 +40,20 @@ void loop() {
   // US_ROUNDTRIP_CM = 57 in config
   // Approximation to shut off motor once within about 5 cm
   if(distance < 150){ 
-    pwmWrite(motor, 0);
+    analogWrite(motor, 0);
   }
   // Values need to be tweaked - dependent on frequency of PWM
   else if(distance >= 150 && distance < 250)
-    pwmWrite(motor, 150);
+    analogWrite(motor, 150);
   else if(distance >= 250 && distance < 350)
-    pwmWrite(motor, 175);
+    analogWrite(motor, 175);
   else if(distance >= 350 && distance < 500)
-    pwmWrite(motor, 200);
+    analogWrite(motor, 200);
   else if(distance >= 500 && distance < 700)
-    pwmWrite(motor, 240);
+    analogWrite(motor, 240);
   else if(distance >= 700 && distance < 1000)
-    pwmWrite(motor, 255);
+    analogWrite(motor, 255);
   else 
-    pwmWrite(motor, 255);
+    analogWrite(motor, 255);
 }
 
